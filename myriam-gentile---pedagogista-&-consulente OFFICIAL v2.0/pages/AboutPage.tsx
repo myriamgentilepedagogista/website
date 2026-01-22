19f2303897c91001ab6fdad685a0f10a3f72050e
myriamgentilepedagogista <myriamgentilepedagogista@outlook.it>
2026-01-22 16:33:07 +0100

    Update AboutPage.tsx

--- myriam-gentile---pedagogista-&-consulente OFFICIAL v2.0/pages/AboutPage.tsx
+++ myriam-gentile---pedagogista-&-consulente OFFICIAL v2.0/pages/AboutPage.tsx
@@ -5,7 +5,7 @@ import { Link, useLocation } from 'react-router-dom';
 import SEO from '../components/SEO';
 import { getMeta } from '../seo/metadata';
 
-const PROFILE_IMAGE_URL = "https://media.licdn.com/dms/image/v2/D4D03AQFffN7uEwAnZw/profile-displayphoto-shrink_800_800/B4DZaXRjYPGsAc-/0/1746294684622?e=1769040000&v=beta&t=IxsX1icutmnLpBNRhmsQQe5CPC7Zoiy7KGOeiuuGljU"; 
+const PROFILE_IMAGE_URL = "https://i.imgur.com/bGfsC7Y.jpeg"; 
 
 const AboutPage: React.FC = () => {
   const location = useLocation();
